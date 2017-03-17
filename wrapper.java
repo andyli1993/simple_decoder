28b58e9113f629cbe2275095cfc88427ddf9ebb5
andyli1993 <zcqsgli@ucl.ac.uk>
2017-03-17 20:39:01 +0000

    final official
    rot13

--- wrapper.java
+++ wrapper.java
@@ -17,30 +17,25 @@ public class wrapper {
 		// instantiate a hash map
 		HashMap<Character,Character> library = new HashMap<Character,Character>() ;
 		// Put elements to the map, ideally should be stored elsewhere and imported using a secured reader.
-		library.put('a', 'n');
-		library.put('b', 'o');
-		library.put('c', 'p');
-		library.put('d', 'q');
-		library.put('e', 'r');
-		library.put('f', 's');
-		library.put('g', 't');
-		library.put('h', 'u');
-		library.put('i', 'v');
-		library.put('j', 'w');
-		library.put('k', 'x');
-		library.put('l', 'y');
-		library.put('m', 'z');
-		
+
+		//create a loop which records the decoder value pair onto the hashmap
+
+		for (char alphabet1 = 'a', alphabet2 = 'n';	alphabet1 <= 'm'; 	alphabet1++, alphabet2++) {
+			library.put(alphabet1,alphabet2);
+
+		}
+
+		//prompt message to ask ur
 		System.out.print("Enter your string want to be decoded: ");
 		//  read input
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		String input_string = null;
 
-	
+
 		//store input
 		try {
 			input_string = br.readLine();	
-			
+
 		} catch (IOException ioe) {
 			System.out.println("please check your input");
 			System.exit(1);
@@ -50,37 +45,38 @@ public class wrapper {
 		String processed =process(input_string);
 		boolean abc= true;
 		size= processed.length();
-        //convert string to separate tokens 
-		char[] ab=processed.replaceAll("\\W","").toCharArray();		
+		//convert string to separate tokens 
+		char[] tokens=processed.replaceAll("\\W","").toCharArray();		
 		StringBuilder decoded_code = new StringBuilder();	
 		int counter =1;	
 		//loop through and append decoded char
 		while(size>0){
-			
-			if (library.get(ab[counter-1]) == null) {
-				System.out.println("error spotted!");
+
+			if (library.get(tokens[counter-1]) == null) {
+				System.out.println("can't decode that!");
 				abc=false;
 				break;
-				
-			  
+
+
 			} else {			  	
-			decoded_code.append(library.get(ab[counter-1]));
-	      	counter++;
-			size--;
+				decoded_code.append(library.get(tokens[counter-1]));
+				counter++;
+				size--;
 			}
 		}
 
 		if(abc!=false){
-		System.out.println("you decoded code is :" +decoded_code);
+			System.out.println("you decoded code is :" +decoded_code);
+
 		}
-		}
-	
+	}
+
 	//process data based on future requirements
 	public static String process(String input_string) {
-		//two assumptions input string are not trimmed, and are not all lower case, alphabets input only limited to m 
-		String processed=input_string.trim();
-		processed.toLowerCase();	
-		return processed;		
+		//two assumptions and are not all lower case, alphabets input only limited to m, user might put invalid input by mistake in which case system will trigger an error message
+
+
+		return input_string.toLowerCase();			
 	}
 }
 
